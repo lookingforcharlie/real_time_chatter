a8e852db6661c7079ae4157e5fd44c2afb8863c5
lookingforcharlie <charliefeng2020@gmail.com>
2023-06-28 01:23:39 -0400

    security

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -13,23 +13,10 @@ const page = async () => {
   console.log('Next auth session got from dashboard page.tsx:', session);
   console.log('--------------------');
 
-  return (
-    // <div className='flex flex-col gap-6'>
-    //   <h1 className='text-4xl'>Dashboard</h1>
-    //   <div>You are logged in successfully</div>;
-    //   <Link
-    //     href='/dashboard/add'
-    //     className='border border-orange-500 px-4 py-2'
-    //   >
-    //     Go to add friend
-    //   </Link>
-    // <pre>{JSON.stringify(session)}</pre>
-    // </div>
-    <div>Dashboard</div>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default page;
 
 // this is how session looks like
-// {"user":{"name":"Bino Feng","email":"bino.feng@gmail.com","image":"https://lh3.googleusercontent.com/a/AAcHTtcaHUWMAbGjtx-V98U36E3lC2ngdUa40XW1TwB-=s96-c","id":"6543b13d-021c-43fc-835b-be014c98dc2c"}}
+// {"user":{"name":"Bino Feng","email":"bino.feng@gmail.com","image":"https://lh3.googleusercontent.com/a/AAcHTtcaHUWfewaYBGHHYAbGaefaadsfas3lC2ngdUa4B-=s96-c","id":"6543b13d-adsefawef-wefe--be014c98dc2c"}}
