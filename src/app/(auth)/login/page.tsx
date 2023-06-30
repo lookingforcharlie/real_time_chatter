d4921de82a4321d73ffcbc6d01e86fa75dcf1c31
lookingforcharlie <charliefeng2020@gmail.com>
2023-06-30 02:29:20 -0400

    implement login page animation

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -21,62 +21,70 @@ const Page: FC = () => {
   }
   return (
     <>
-      <div className='flex min-h-full mt-20 items-center justify-center  py-12 px-4 sm:px-6 lg:px-8'>
-        <div className='w-full flex flex-col border border-gray-400 rounded-md shadow-lg px-20 py-20 items-center max-w-md space-y-8'>
-          <div className='flex flex-col items-center gap-8 '>
-            <h2 className='text-center text-4xl font-bold tracking-tight text-gray-900'>
-              Let&apos;s chat ...
-            </h2>
-            <div>
-              <Image
-                src='/chatter_logo.png'
-                height={200}
-                width={200}
-                alt='App Logo'
-              />
+      <div className='bg-gray-100 flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+        <div className='relative w-auto max-w-lg'>
+          {/* Start to build the bg effect */}
+          <div className='absolute top-0 -left-8 w-80 h-80 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob'></div>
+          <div className='absolute top-0 -right-8 w-80 h-80 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-2000'></div>
+          <div className='absolute top-6 left-20 w-80 h-80 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-4000'></div>
+
+          {/* Login section */}
+          <div className='bg-zinc-200 flex flex-col border-gray-400 rounded-md shadow-lg px-24 py-20 items-center max-w-md space-y-8'>
+            <div className='flex flex-col items-center gap-8 '>
+              <h2 className='text-center text-4xl font-bold tracking-tight text-gray-900'>
+                Let&apos;s chat ...
+              </h2>
+              <div>
+                <Image
+                  src='/chatter_logo.png'
+                  height={200}
+                  width={200}
+                  alt='App Logo'
+                />
+              </div>
+              <h2 className='mt-6 text-center text-2xl font-bold tracking-tight text-gray-900'>
+                Sign in to your account
+              </h2>
             </div>
-            <h2 className='mt-6 text-center text-2xl font-bold tracking-tight text-gray-900'>
-              Sign in to your account
-            </h2>
+            <Button
+              isLoading={isLoading}
+              type='button'
+              className='max-w-xl mx-auto w-full py-6'
+              onClick={loginWithGoogle}
+            >
+              {isLoading ? null : (
+                <svg
+                  className='mr-2 h-4 w-4'
+                  aria-hidden='true'
+                  focusable='false'
+                  data-prefix='fab'
+                  data-icon='github'
+                  role='img'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                >
+                  <path
+                    d='M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z'
+                    fill='#4285F4'
+                  />
+                  <path
+                    d='M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z'
+                    fill='#34A853'
+                  />
+                  <path
+                    d='M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z'
+                    fill='#FBBC05'
+                  />
+                  <path
+                    d='M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z'
+                    fill='#EA4335'
+                  />
+                  <path d='M1 1h22v22H1z' fill='none' />
+                </svg>
+              )}
+              Google
+            </Button>
           </div>
-          <Button
-            isLoading={isLoading}
-            type='button'
-            className='max-w-md mx-auto w-full'
-            onClick={loginWithGoogle}
-          >
-            {isLoading ? null : (
-              <svg
-                className='mr-2 h-4 w-4'
-                aria-hidden='true'
-                focusable='false'
-                data-prefix='fab'
-                data-icon='github'
-                role='img'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  d='M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z'
-                  fill='#4285F4'
-                />
-                <path
-                  d='M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z'
-                  fill='#34A853'
-                />
-                <path
-                  d='M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z'
-                  fill='#FBBC05'
-                />
-                <path
-                  d='M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z'
-                  fill='#EA4335'
-                />
-                <path d='M1 1h22v22H1z' fill='none' />
-              </svg>
-            )}
-            Google
-          </Button>
         </div>
       </div>
     </>
